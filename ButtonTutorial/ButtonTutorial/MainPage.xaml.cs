4151dee515826a074d2c05c25d9af6a9649c9fa9
User <User@WIN-C9HQIV4VA1U>
2021-12-07 22:00:30 +0300

    Add project files.

---
+++ ButtonTutorial/ButtonTutorial/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ButtonTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
